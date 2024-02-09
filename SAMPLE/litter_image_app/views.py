0538791cc970cc6e258e98577ccc513a6a674bd0
Hiroaki Ozasa <37623652+OzasaHiro@users.noreply.github.com>
2024-02-08 22:55:05 -0800

    Add files via upload

--- SAMPLE/litter_image_app/views.py
+++ SAMPLE/litter_image_app/views.py
@@ -5,11 +5,13 @@ from io import BytesIO
 import numpy as np
 import tensorflow as tf
 from rembg import remove
+import datetime
+import os
 
-# クラスの定義
+# class definition
 CLASSES = ['cardboard','compost', 'glass', 'metal', 'paper',  'plastic', 'trash']
 
-# TensorFlow Liteモデルの読み込み
+# Load TensorFlow Lite model
 interpreter = tf.lite.Interpreter(model_path="littersort_model.tflite")
 interpreter.allocate_tensors()
 
@@ -18,27 +20,36 @@ interpreter.allocate_tensors()
 def upload(request):
     if request.method == 'POST' and request.FILES.get('image'):
         image_file = request.FILES['image']
-        image = Image.open(image_file)
+        original_image = Image.open(image_file)
 
-        # リサイズと背景除去
-        image = image.resize((224, 224))
+        #Resize and remove-background
+        image = original_image.resize((224, 224))
         image = remove(image)
 
-        # RGBAからRGBへの変換
+        # convert from RGBA to RGB
         if image.mode == 'RGBA':
             r, g, b, a = image.split()
             image = Image.merge("RGB", (r, g, b))
 
-        # 画像分類
+        # Image Classification
         class_name, confidence = classify_image(image, interpreter)
         confidence = float(confidence)
+
+        # Create save dir
+        os.makedirs("uploads", exist_ok=True)
+
+        now = datetime.datetime.now()
+        filename = f"{class_name}_{now:%Y%m%d%H%M%S}.jpg"
+
+        # save uploaded image
+        original_image.save(f"uploads/{filename}")
         
         return JsonResponse({'class': class_name, 'confidence': confidence})
 
     return JsonResponse({'error': 'Invalid request'}, status=400)
 
 def classify_image(img, interpreter):
-    # 画像の前処理
+    # pre processing
     img_arr = np.array(img).astype(np.float32) / 255.0
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
